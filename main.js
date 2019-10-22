319b50011a6f1f8509d65dd221f5c9dc8fbf1437
Anathi Anathi <anathi.gumede@codeimmersives.com>
2019-10-22 16:01:33 -0400

    attempt at absolute values

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-function doubleAll() {
+function doubleAll(numbers) {
+  const twiceOver = [];
+    for (let i = 0; i < numbers.length; i++) {
+      twiceOver.push(numbers[i] * 2);
+    }
+  return twiceOver;
 }
 
-function absoluteValues() {
+function absoluteValues(maxim) {
+  const abso = [];
+  for (let i = 0; i < maxim.length; i++) {
+    if (maxim[i] === )
+  }
+  {
+    if (maxim[i] === maxim[i]) {
+      abso.push(maxim[i])
+    }
+  }
+  return abso;
 }
 
-function yelledGreetings() {
+function yelledGreetings(oldYeller) {
+  //const shout = [];
+  // for (let i = 0; i < oldYeller.length; )
 }
 
 function changeToInitials() {
